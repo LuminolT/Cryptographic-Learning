38e74ccae53a279832163e7baf9c8c3475ef7727
LuminolT <copper_sulfate@qq.com>
2022-05-23 18:56:26 +0800

    Update: Calc Hamming Distance

--- Coding Theory/Lecture-5-Coding-Basic/hamming_distance.py
+++ Coding Theory/Lecture-5-Coding-Basic/hamming_distance.py
@@ -1,17 +1,22 @@
 from math import floor
 
-def find_minimun_distance(C: list[int])->int:
+
+def find_minimun_distance(C: list[int]) -> int:
     res = bin(C[0] ^ C[1]).count("1")
     for i in range(len(C)):
         for j in range(i+1, len(C)):
+            print(f'd_{i+1}_{j+1} := {bin(C[i] ^ C[j])}')
             res = min(res, bin(C[i] ^ C[j]).count("1"))
     return(res)
 
+
 def main():
     # C = [0b10010, 0b01001, 0b10101, 0b01110]
-    C = [0b101010, 0b101101, 0b011011, 0b001110]
+    # C = [0b101010, 0b101101, 0b011011, 0b001110]
+    C = [0b10100, 0b01010, 0b01101, 0b10011]
     min_dis = find_minimun_distance(C)
-    print(f"min_dis: {min_dis}, detect: {min_dis - 1}, correct: {floor((min_dis - 1)/2)}")
+    print(
+        f"min_dis: {min_dis}, detect: {min_dis - 1}, correct: {floor((min_dis - 1)/2)}")
 
 
 if __name__ == "__main__":
